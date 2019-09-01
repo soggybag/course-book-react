881129f0ed919a5d47e53dc9d8ea7026fa809608
Mitchell <soggybag@gmail.com>
2019-08-31 21:38:43 -0700

    Updates

---
+++ precache-manifest.18f7264fad52a4faad8deb24be299d55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1ec53a2a0e9088c26823077c651cab",
+    "url": "/course-book-react/index.html"
+  },
+  {
+    "revision": "44f5a589995204f14d10",
+    "url": "/course-book-react/static/css/main.3a10d240.chunk.css"
+  },
+  {
+    "revision": "de6dbe846663a305b140",
+    "url": "/course-book-react/static/js/2.5fa144cb.chunk.js"
+  },
+  {
+    "revision": "44f5a589995204f14d10",
+    "url": "/course-book-react/static/js/main.dcc5c259.chunk.js"
+  },
+  {
+    "revision": "daf77401d2a3b2e47666",
+    "url": "/course-book-react/static/js/runtime~main.cf17ea87.js"
+  }
+]);
