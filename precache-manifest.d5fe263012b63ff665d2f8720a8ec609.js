a83743bb8071cd2e05c4f7b51223ac55e3d4a091
Mitchell <soggybag@gmail.com>
2020-09-30 15:45:04 -0700

    Updates

--- precache-manifest.d5fe263012b63ff665d2f8720a8ec609.js
+++ precache-manifest.d5fe263012b63ff665d2f8720a8ec609.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7fed51750aaa7dd0e0d22994f66647bf",
+    "revision": "2b47ebaa6c2a2e1fc665bd13fd674d4e",
     "url": "/course-book-react/index.html"
   },
   {
-    "revision": "bfcdd2769eb9945a9ab8",
-    "url": "/course-book-react/static/css/main.3a10d240.chunk.css"
+    "revision": "13810f47a3afa9b990fe",
+    "url": "/course-book-react/static/css/main.9e23294e.chunk.css"
   },
   {
     "revision": "de6dbe846663a305b140",
     "url": "/course-book-react/static/js/2.5fa144cb.chunk.js"
   },
   {
-    "revision": "bfcdd2769eb9945a9ab8",
-    "url": "/course-book-react/static/js/main.1ae6907e.chunk.js"
+    "revision": "13810f47a3afa9b990fe",
+    "url": "/course-book-react/static/js/main.1453e60a.chunk.js"
   },
   {
     "revision": "daf77401d2a3b2e47666",
